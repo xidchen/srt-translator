c77855f695d1b6da9728b52666ebdedb6e808f07
Sheldon Chen <xidchen.work@gmail.com>
2024-04-10 19:43:33 +0800

    Add cfg.py

---
+++ cfg.py
@@ -0,0 +1,10 @@
+input_dir = "subtitles"
+output_dir = "subtitles_translated"
+output_lang = ["en", "zh-TW"]
+input_srt_suffix = ".srt"
+output_srt_suffix = "_translated.srt"
+
+google_translate = "https://translate.google.com/m?"
+proxy_address = "127.0.0.1:25378"
+remote_api_timeout = 10
+translate_error = "Translation failed"
