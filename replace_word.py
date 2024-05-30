1ddde613fc9986421eaf96728f4927810dcb7edf
Sheldon Chen <xidchen.work@gmail.com>
2024-05-30 10:07:48 +0800

    Update replace_word.py

--- replace_word.py
+++ replace_word.py
@@ -9,6 +9,7 @@ replacement_dict = {
 
 
 def process(srt_dir):
+    count = 0
     for dir_path, _, file_names in os.walk(srt_dir):
         for file_name in file_names:
             if file_name.endswith(cfg.output_srt_suffix):
@@ -16,9 +17,11 @@ def process(srt_dir):
                 with open(srt_file, "r", encoding="utf-8") as f:
                     content = f.read()
                 for old, new in replacement_dict.items():
+                    count += content.count(old)
                     content = content.replace(old, new)
                 with open(srt_file, "w", encoding="utf-8") as f:
                     f.write(content)
+    print(f"Count: {count}")
 
 
 if __name__ == "__main__":
