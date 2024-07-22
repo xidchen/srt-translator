641d60eff67eb5985992a8dd1fe86a55696bfcf5
Sheldon Chen <xidchen.work@gmail.com>
2024-07-22 23:11:07 +0800

    Add single and double enter

--- cfg.py
+++ cfg.py
@@ -1,10 +1,13 @@
-input_dir = "subtitles"
-output_dir = "subtitles_translated"
-output_lang = ["en", "zh-TW"]
-input_srt_suffix = ".srt"
+input_dir = "subtitles_en"
+output_dir = "subtitles_pt"
+input_lang = "en"
+output_lang = ["pt-PT"]
+input_srt_suffix = "_translated.srt"
 output_srt_suffix = "_translated.srt"
 
 google_translate = "https://translate.google.com/m?"
+single_enter = "\n"
+double_enter = "\n\n"
 remote_api_timeout = 10
 translate_error = "Translation failed"
 time_to_sleep = 1
